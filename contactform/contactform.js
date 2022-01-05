767bc4ce0d4fa60bf8c0dc0843f10eb3d34a802b
heros20 <heros40@hotmail.fr>
2022-01-05 12:32:49 +0100

    sending mail

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -90,28 +90,28 @@ jQuery(document).ready(function($) {
     });
     if (ferror) return false;
     else var str = $(this).serialize();
-    // var action = $(this).attr('action');
+    var action = $(this).attr('action');
     // if( ! action ) {
     //   action = 'contactform/contactform.php';
     // }
-    // $.ajax({
-    //   type: "POST",
-    //   url: action,
-    //   data: str,
-    //   success: function(msg) {
-    //     // alert(msg);
-    //     if (msg == 'OK') {
-    //       $("#sendmessage").addClass("show");
-    //       $("#errormessage").removeClass("show");
-    //       $('.contactForm').find("input, textarea").val("");
-    //     } else {
-    //       $("#sendmessage").removeClass("show");
-    //       $("#errormessage").addClass("show");
-    //       $('#errormessage').html(msg);
-    //     }
+    $.ajax({
+      type: "POST",
+      url: action,
+      data: str,
+      // success: function(msg) {
+      //   // alert(msg);
+      //   if (msg == 'OK') {
+      //     $("#sendmessage").addClass("show");
+      //     $("#errormessage").removeClass("show");
+      //     $('.contactForm').find("input, textarea").val("");
+      //   } else {
+      //     $("#sendmessage").removeClass("show");
+      //     $("#errormessage").addClass("show");
+      //     $('#errormessage').html(msg);
+      //   }
 
-    //   }
-    // });
+      // }
+    });
     return false;
   });
 
